374a88e19512e6b74d4edab2abbb997a4b936403
Adam Gourley <75768388+MiniHood@users.noreply.github.com>
2026-01-19 06:02:05 +0000

    feat: Add ARGH Ambient Vehicle Catalog Bootstrap Component

---
+++ Scripts/Game/ARGH/Server/Components/ScriptComponent/ARGH_AmbientVehicleCatalogBootstrapComponent.c
@@ -0,0 +1,12 @@
+class ARGH_AmbientVehicleCatalogBootstrapComponentClass : ScriptComponentClass
+{
+}
+
+class ARGH_AmbientVehicleCatalogBootstrapComponent : ScriptComponent
+{
+	override void OnPostInit(IEntity owner)
+	{
+		super.OnPostInit(owner);
+
+	}
+}
